ff9748e4d3bfba06b63106014dce192b237c28fa
Devendra Kumar <deven830550@gmail.com>
2025-08-19 15:33:58 +0530

    issue Shop.jsx

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Component/Headers/Header";
 import Newsletter from "../Component/Newsletters/Newsletter";
 import Footer from "../Component/Footers/Footer";
-import PageHeader from "../Component/Shop/pageHeader";
+import PageHeader from "../Component/Shop/PageHeader";
 import ProductPagination from "../Component/Products/ProductPagination";
 
 const Shop = () => {
